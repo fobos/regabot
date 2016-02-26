c5f6c2d58a6f1450cc7bcc65cbb93fdfafd6e312
Alexander Biryukov <alex.biryukoff@gmail.com>
2016-02-27 01:37:36 +0600

    Initial commit. Create simple regaBot

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const bot = require('./regaCounterBot');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/message',
+    handler: (request, reply) => {
+        reply();
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/rega',
+    handler: bot
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
